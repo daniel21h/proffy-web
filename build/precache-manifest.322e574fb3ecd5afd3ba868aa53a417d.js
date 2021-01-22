26f7597c6e7deea06d30fda21db55f545832da99
daniel21h <hesseldaniel21@gmail.com>
2021-01-22 16:12:38 -0300

    feat: add build

---
+++ build/precache-manifest.322e574fb3ecd5afd3ba868aa53a417d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0209dee49300b2552bdb35d74fc6a8f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a09403c1c074a37642c4",
+    "url": "/static/css/main.aa8cea03.chunk.css"
+  },
+  {
+    "revision": "2b32bb73bf3326de9e4f",
+    "url": "/static/js/2.e4fdb68a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e4fdb68a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a09403c1c074a37642c4",
+    "url": "/static/js/main.473ca1ab.chunk.js"
+  },
+  {
+    "revision": "d0a598c2c4f910599d51",
+    "url": "/static/js/runtime-main.e7310a31.js"
+  },
+  {
+    "revision": "1fbb1f8bd147351e311921cdf4e80dc3",
+    "url": "/static/media/back.1fbb1f8b.svg"
+  },
+  {
+    "revision": "9ed76be57e0f41fb4092cbbcb15e0875",
+    "url": "/static/media/give-classes.9ed76be5.svg"
+  },
+  {
+    "revision": "c86272eb642793deb29b7eec180c05a5",
+    "url": "/static/media/landing.c86272eb.svg"
+  },
+  {
+    "revision": "1f9a96a14f86a1ccd682159fe038774d",
+    "url": "/static/media/logo.1f9a96a1.svg"
+  },
+  {
+    "revision": "e042bb237d1a08daabd12bf0a4fb9780",
+    "url": "/static/media/purple-heart.e042bb23.svg"
+  },
+  {
+    "revision": "3c22fb01ba7434dfa52b083a67158019",
+    "url": "/static/media/study.3c22fb01.svg"
+  },
+  {
+    "revision": "744fb7ba0c3df74462352a3858badeb6",
+    "url": "/static/media/warning.744fb7ba.svg"
+  },
+  {
+    "revision": "c2ffa8a6a2f6690a5eba826c625715ab",
+    "url": "/static/media/whatsapp.c2ffa8a6.svg"
+  }
+]);
