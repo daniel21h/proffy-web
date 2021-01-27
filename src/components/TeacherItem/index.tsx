7745659deae46089fed36dda1073c300cbe8aef7
daniel21h <hesseldaniel21@gmail.com>
2021-01-27 08:51:30 -0300

    fix: solving bug

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -52,6 +52,7 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
 
         <a 
           target="_blank" 
+          rel="noopener noreferrer"
           onClick={createNewConnection} 
           href={`https://wa.me/${teacher.whatsapp}`}
         >
